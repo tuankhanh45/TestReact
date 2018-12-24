196f544a602690788437f3c1f17fe326a86b3141
unknown <tuankhanhtran45@gmail.com>
2018-12-24 15:38:42 +0700

    done router

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import HomePage from './pages/HomePage/HomePage';
 import NotFound from './pages/NotFound/404Page';
 import ProductListPage from './pages/ProductListPage/ProductListPage'
-
+import ProductActionPage from './pages/ProductActionPage/ProductActionPage'
 const routes = [
     {
         path: '/',
@@ -15,9 +15,21 @@ const routes = [
         main: () => <ProductListPage />
     },
     {
+        path: '/product/add',
+        exact: false,
+        main: () => <ProductActionPage />
+    },
+    {
+        path: '/product/:id/edit',
+        exact: false,
+        main: ({ match }) => <ProductActionPage match={match} />
+    },
+
+    //add new route before page not found
+    {
         path: '',
         exact: false,
         main: () => <NotFound />
-    },
+    }
 ];
 export default routes;
